e5b98eae834b8c146b314a82db83453a18a5021c
renatosouzaBR <natoicm@gmail.com>
2022-05-22 18:34:55 -0300

    Add theme

--- App.tsx
+++ App.tsx
@@ -1,7 +1,52 @@
-import React from "react";
+import React, { useEffect } from "react";
+import * as SplashScreen from "expo-splash-screen";
+import { ThemeProvider } from "styled-components";
+import {
+  useFonts,
+  Archivo_400Regular,
+  Archivo_500Medium,
+  Archivo_600SemiBold,
+} from "@expo-google-fonts/archivo";
+import {
+  Inter_400Regular,
+  Inter_500Medium,
+  Inter_600SemiBold,
+} from "@expo-google-fonts/inter";
 
 import { Home } from "./src/screens/Home";
+import theme from "./src/global/styles/theme";
 
 export default function App() {
-  return <Home />;
+  const [fontsLoaded] = useFonts({
+    Archivo_400Regular,
+    Archivo_500Medium,
+    Archivo_600SemiBold,
+    Inter_400Regular,
+    Inter_500Medium,
+    Inter_600SemiBold,
+  });
+
+  async function prepare() {
+    await SplashScreen.preventAutoHideAsync();
+  }
+
+  async function showApp() {
+    await SplashScreen.hideAsync();
+  }
+
+  useEffect(() => {
+    prepare();
+  }, []);
+
+  useEffect(() => {
+    if (fontsLoaded) {
+      showApp();
+    }
+  }, [fontsLoaded]);
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Home />
+    </ThemeProvider>
+  );
 }
---
+++ src/global/styles/theme.ts
@@ -0,0 +1,28 @@
+export default {
+  colors: {
+    primary: "#DC1637",
+    primary_light: "#FF99AA",
+    dark: "#1B1B1F",
+
+    shape: "#29292E",
+    background: "#FFFFFF",
+    success: "#03B252",
+
+    title: "#47474D",
+    text: "#7A7A80",
+    text_light: "#AEAEB3",
+  },
+
+  fonts: {
+    inter: {
+      regular: "Inter_400Regular",
+      medium: "Inter_500Medium",
+      semiBold: "Inter_600SemiBold",
+    },
+    archivo: {
+      regular: "Archivo_400Regular",
+      medium: "Archivo_500Medium",
+      semiBold: "Archivo_600SemiBold",
+    },
+  },
+};
